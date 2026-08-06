58de3f63882c4ed8d4a2d438159111e1dd3893cc
Kori Tk <66495933+hyprtk@users.noreply.github.com>
2026-08-06 10:06:37 +0100

    update

--- installer/scripts/keyhint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# ── Keyboard Shortcuts ────────────────────────────────
-# by Kori Tk (2026)
-# ─────────────────────────────────────────────────────
-
-alacritty -e micro ~/hyprtk/cheatsheet.md
--- installer/scripts/octopi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# ── Octopi ────────────────────────────────────────────
-# by Kori Tk (2026)
-# ─────────────────────────────────────────────────────
-
-pkill octopi-notifier
-
-/usr/bin/octopi-notifier &
